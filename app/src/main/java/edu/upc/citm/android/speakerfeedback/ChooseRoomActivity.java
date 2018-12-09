bad6be7498b5bfc68392765f51326865c46f8ed3
Pau Fernández <pau.fernandez@upc.edu>
2018-12-09 13:49:49 +0100

    Afegeixo l'activitat principal que permet escollir rooms i crear-ne.

---
+++ app/src/main/java/edu/upc/citm/android/speakerfeedback/ChooseRoomActivity.java
@@ -0,0 +1,98 @@
+package edu.upc.citm.android.speakerfeedback;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChooseRoomActivity extends AppCompatActivity {
+
+    private RecyclerView room_list_view;
+
+    private List<Room> rooms;
+    private Adapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_room);
+
+        rooms = new ArrayList<>();
+        rooms.add(new Room("testroom"));
+        rooms.add(null);
+
+        room_list_view = findViewById(R.id.room_list_view);
+        room_list_view.setLayoutManager(new GridLayoutManager(this, 2));
+        adapter = new Adapter();
+        room_list_view.setAdapter(adapter);
+    }
+
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+        CardView card_view;
+        LinearLayout add_room_view;
+        TextView room_id_view;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            card_view = itemView.findViewById(R.id.card_view);
+            add_room_view = itemView.findViewById(R.id.add_room_view);
+            room_id_view = itemView.findViewById(R.id.room_id_view);
+
+            card_view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    onRoomClick(getAdapterPosition());
+                }
+            });
+        }
+    }
+
+    private void onRoomClick(int pos) {
+        Toast.makeText(this, "Clicked room " + pos, Toast.LENGTH_SHORT).show();
+    }
+
+    class Adapter extends RecyclerView.Adapter<ViewHolder> {
+
+        @NonNull
+        @Override
+        public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View itemView = getLayoutInflater().inflate(R.layout.room_view, parent, false);
+            return new ViewHolder(itemView);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+            Room room = rooms.get(position);
+            if (room == null) {
+                // Un room buit significa el botó d'afegir room!
+                holder.add_room_view.setVisibility(View.VISIBLE);
+                holder.room_id_view.setVisibility(View.GONE);
+                holder.card_view.setCardElevation(0.0f);
+                holder.card_view.setCardBackgroundColor(0xffeeeeee);
+                return;
+            }
+
+            holder.card_view.setCardElevation(5.0f);
+            holder.card_view.setCardBackgroundColor(0xffffffff);
+            holder.add_room_view.setVisibility(View.GONE);
+            holder.room_id_view.setText(rooms.get(position).getId());
+        }
+
+        @Override
+        public int getItemCount() {
+            return rooms.size();
+        }
+    }
+
+}
---
+++ app/src/main/java/edu/upc/citm/android/speakerfeedback/Room.java
@@ -0,0 +1,17 @@
+package edu.upc.citm.android.speakerfeedback;
+
+class Room {
+    private String id;
+
+    public Room(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
